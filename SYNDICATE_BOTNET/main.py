bf1052a0e1a4c21893bf0f0e4429787be6997af7
lutfiix77-commits <lutfiix77@gmail.com>
2026-02-16 04:46:16 +0700

    Update main.py

--- SYNDICATE_BOTNET/main.py
+++ SYNDICATE_BOTNET/main.py
@@ -1,1 +1,15 @@
+import sys
+import os
+from pathlib import Path
 
+package_dir = Path(__file__).parent.absolute()
+if str(package_dir) not in sys.path:
+    sys.path.append(str(package_dir)) 
+try:
+    from . import run
+except ImportError:
+    import run
+def start():
+    run.execute()
+if __name__ == "__main__":
+    start()
